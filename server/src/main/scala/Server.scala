089b785cafb28c5e51b04e884fd6a17f9c262e6c
François David <francois.david@ibm.com>
2018-06-02 11:05:35 +0200

    [SERV][ROUTE] Create a first test route & Actor system

---
+++ server/src/main/scala/Server.scala
@@ -0,0 +1,44 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import akka.Done
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+
+
+import scala.io.StdIn
+
+import scala.concurrent.Future
+
+object Server {
+
+  // needed to run the route
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  // needed for the future map/flatmap in the end and future in fetchItem and saveOrder
+  implicit val executionContext = system.dispatcher
+
+
+
+  def main(args: Array[String]) {
+
+    val route: Route =
+      get {
+        pathPrefix("test" ) {
+          println("test route")
+          complete(StatusCodes.OK)
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ ⇒ system.terminate()) // and shutdown when done
+
+  }
+}
