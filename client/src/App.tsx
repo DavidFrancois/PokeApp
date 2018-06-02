872845e16429b6c74124c49abcfc2b4c39820c5f
François David <francois.david@ibm.com>
2018-06-02 21:50:57 +0200

    [CLIENT][DEP] Updated Home Component dependencies and deleted old files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
-import { Home } from './components/home/home';
+import { Login } from './components/home/home';
 
 // import logo from './logo.svg';
 
@@ -10,7 +10,7 @@ class App extends React.Component {
   public render() {
     return (
       <Switch>
-        <Route exact={true} path='/' component={Home}/>
+        <Route exact={true} path='/' component={Login}/>
       </Switch>
     );
   }
