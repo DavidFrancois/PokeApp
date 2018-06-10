6ac3937351b96ca81b7e679bbbcb0079daedab80
François David <francois.david@ibm.com>
2018-06-10 20:01:26 +0200

    [CLIENT][FETCH] App.tsx now add pokemons to the store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,29 +6,23 @@ import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import { Home } from './components/home/home';
 import { Login } from './components/login/login';
 
+import pokeStore from './stores/pokestore';
 
-
-// import logo from './logo.svg';
-
-class App extends React.Component<{}, { pokemons: string[] }> {
+class App extends React.Component {
 
   constructor(props: any) {
     super(props);
     this.fetchPokemons = this.fetchPokemons.bind(this);
-    this.state = { pokemons: [] };
   }
 
-  public componentDidMount() {
+  public componentWillMount() {
     this.fetchPokemons();
   }
 
   public async fetchPokemons(queryParams: string = "?limit=200") {
     PokeService.getPokemon(queryParams)
       .then(data => {
-        this.setState((prev) => ({
-           pokemons: prev.pokemons.concat(data.results.map((r: any) => r.name))
-        }));
-
+        pokeStore.addPokemons(data.results.map((r: any) => r.name));
         if (data.next != null) {
           const next = data.next.split('/');
           this.fetchPokemons(next[next.length - 1]);
@@ -42,7 +36,7 @@ class App extends React.Component<{}, { pokemons: string[] }> {
         {/* Router only support one child element, so wrap routes in a div */}
         <div>
           <Route  path='/login' component={Login}/>
-          <Route path='/' exact={true} component={Home}/>
+          <Route path='/' exact={true} component={Home} />
         </div>
       </Switch>
     );
