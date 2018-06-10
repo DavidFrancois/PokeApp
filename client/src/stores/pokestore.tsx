7f17e0b437042baa8d7309dfe39f01d8e5928d53
François David <francois.david@ibm.com>
2018-06-10 20:00:39 +0200

    [CLIENT][STORE] Created Pokemon Store

---
+++ client/src/stores/pokestore.tsx
@@ -0,0 +1,29 @@
+
+class PokeStore {
+  private pokemons: string[];
+  private listeners: any[] = [];
+
+  constructor (initialState: string[]) {
+    this.pokemons = initialState;
+  }
+
+  // TS Lint forbid use of Function type
+  public addListener(l: any) {
+    this.listeners.push(l);
+  }
+
+  // Emit only newly added pokemons or ALL pokemons ?
+  public addPokemons(pokemons: string[]) {
+    this.pokemons.concat(pokemons);
+    for (const listener of this.listeners) {
+      listener(pokemons);
+    }
+  }
+
+  public getState () {
+    return this.pokemons;
+  }
+}
+
+const pokeStore = new PokeStore([]);
+export default pokeStore;
