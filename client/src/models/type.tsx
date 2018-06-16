2ae7233a8b1cb61115c9c79f50c59a173cc4cb5d
François David <francois.david@ibm.com>
2018-06-16 14:42:32 +0200

    [CLIENT][TYP] Updateed type model

--- client/src/models/type.tsx
+++ client/src/models/type.tsx
@@ -1,42 +1,59 @@
 export class PokeType {
-  private statAvg: Map<string, number> = new Map<string, number>();
+  private statAvg: Stats = {
+    "hp": 0,
+    "speed": 0,
+    "attack": 0,
+    "defense": 0,
+    "sp_def": 0,
+    "sp_atk": 0,
+    "weight": 0,
+    "height": 0
+  }
 
   constructor (private name: string) {
     this.name = name;
-    for (const s of Stats)
-      this.statAvg.set(s, 0);
   }
 
-  public setValue(key: string, value: number) {
-    this.statAvg.set(key, value);
+  public setValue(key: string, value: number): void {
+    this.statAvg[key] = value;
   }
 
   public getValue(key: string): number {
-    const value = this.statAvg.get(key);
-    let val: number = 0;
-    if (value) val = value
-    return val;
+    return this.statAvg[key];
   }
 
-  public sumValue(key: string, value: number) {
-    this.statAvg.set(key, this.getValue(key) + value);
+  public sumValue(key: string, value: number): void {
+    this.statAvg[key] += value;
   }
 
-  public getName() {
+  public getName(): string {
     return this.name;
   }
 
-  public getStatMap() {
+  public getStatMap(): Stats {
     return this.statAvg;
   }
 }
 
-export const Stats: string[] = [
+
+export interface Stats {
+  hp?: number,
+  defense?: number,
+  speed?: number,
+  attack?: number
+  sp_def?: number,
+  sp_atk?: number,
+  weight?: number,
+  height?: number
+}
+
+export const StatsKeys = [
   "hp",
   "speed",
   "attack",
+  "defense",
   "sp_def",
   "sp_atk",
   "weight",
   "height"
-]
+];
