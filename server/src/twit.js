5735fa3faaeec75f0188aa453c4c073b9aaabc0e
DavidFrancois <DavidFrancois@users.noreply.github.com>
2018-06-18 08:33:39 +0200

    [SERVER][CRED] Remained part of credentials

--- server/src/twit.js
+++ server/src/twit.js
@@ -4,7 +4,7 @@ const Twit = require('twit');
 const T = new Twit({
   consumer_key:         '',
   consumer_secret:      '',
-  access_token:         '-yk0UrLTVqEbylYt9wAJAqCRWgM1B0dk',
+  access_token:         '',
   access_token_secret:  '',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true     // optional - requires SSL certificates to be valid.
