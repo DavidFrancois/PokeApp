2d220569415b5eb8001b036c34f2d4fcee318d7c
François David <francois.david@ibm.com>
2018-06-19 00:12:00 +0200

    [SERVER] Delete dead code

--- server/src/index.js
+++ server/src/index.js
@@ -3,9 +3,7 @@
 const App = require('./app');
 const http = require('http').Server(App);
 
-// const io = require('./io');
 const port = 8080;
 
-// io.attach(http);
 http.listen(port, () => console.log('Server listening on port: ' + port));
 
